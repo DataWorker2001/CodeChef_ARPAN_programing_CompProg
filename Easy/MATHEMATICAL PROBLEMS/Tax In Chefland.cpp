c2890517f161629ebc83e1977ab2f81b56d4a431
ARPAN CHOUDHURY <123379937+DataWorker2001@users.noreply.github.com>
2023-03-31 07:41:46 +0530

    Add files via upload

---
+++ Easy/MATHEMATICAL PROBLEMS/Tax In Chefland.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int X;
+	    cin>>X;
+	    if(X>100)
+	    cout<<X-10<<endl;
+	    else
+	    cout<<X<<endl;
+	}
+	return 0;
+}
